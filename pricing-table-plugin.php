c560e323de8664f9962d5ff283f13519ece4de3e
Mindaugas Budreika <mindaugas.bu@gmail.com>
2014-02-08 01:40:59 +0400

    * add upgrade to premium link to lefthand nave (like mailchimp for wp)

--- pricing-table-plugin.php
+++ pricing-table-plugin.php
@@ -81,4 +81,21 @@ function dh_ptp_plugin_footer_enqueu($hook_suffix)
 }
 add_action('admin_enqueue_scripts', 'dh_ptp_plugin_footer_enqueu');
 
+/* Upgrade to Premium */
+function dh_ptp_upgrade_to_premium_menu()
+{
+  $page_hook = add_submenu_page('edit.php?post_type=easy-pricing-table', 'Upgrade to Premium', 'Upgrade to Premium', 'manage_options', 'easy-pricing-tables-upgrade', 'dh_ptp_upgrade_to_premium');
+  add_action('load-' . $page_hook , 'dh_ptp_upgrade_ob_start');
+}
+add_action('admin_menu', 'dh_ptp_upgrade_to_premium_menu');
+
+function dh_ptp_upgrade_ob_start() {
+    ob_start();
+}
+
+function dh_ptp_upgrade_to_premium()
+{
+  wp_redirect('http://easypricingtables.com/?utm_source=free-plugin&utm_medium=link&utm_campaign=link-in-left-menu', 301);
+  exit();
+}
 ?>
