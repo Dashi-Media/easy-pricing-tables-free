6529187049870d555220cde87116a60c8b2eb3ea
Mindaugas Budreika <mindaugas.bu@gmail.com>
2014-02-13 00:42:59 +0400

    minor css bug fix

--- includes/table-generation/table-generator.php
+++ includes/table-generation/table-generator.php
@@ -1,44 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: david
- * Date: 11/15/13
- * Time: 15:48
- */
-
 
 // include our HTML generators for each table
-include ( PTP_PLUGIN_PATH . '/includes/table-generation/simple-flat-table.php');
+include ( PTP_PLUGIN_PATH . '/includes/table-generation/design1.php');
 
 /* CSS Styling */
-add_action( 'wp_head', 'dh_ptp_easy_pricing_table_dynamic_css');
-
 function dh_ptp_easy_pricing_table_dynamic_css()
 {
     global $features_metabox;
     
     echo '<style type="text/css">';
-    
-    // Retrieve all meta data for easy pricing tables
-    $args = array(
-        'post_type' => 'easy-pricing-table',
-        'posts_per_page' => -1,
-        'post_status' => 'any'
-    );
-    $query = new WP_Query($args);
-    if ($query->have_posts()) :
-        while ($query->have_posts()) : $query->the_post();
-            $meta = get_post_meta(get_the_ID(), $features_metabox->get_the_id(), true);
-            
-            // Print css style per table
-            dh_ptp_simple_flat_css(get_the_ID(), $meta);
-        endwhile;
-    endif;
-    wp_reset_postdata();
+
+        // Retrieve all meta data for easy pricing tables
+        $query = new WP_Query(array('post_type' => 'easy-pricing-table', 'posts_per_page' => -1, 'post_status' => 'any'));
+        if ($query->have_posts()) :
+            while ($query->have_posts()) : $query->the_post();
+                // Print CSS styles per table
+                $meta = get_post_meta(get_the_ID(), $features_metabox->get_the_id(), true);
+                dh_ptp_simple_flat_css(get_the_ID(), $meta);
+            endwhile;
+        endif;
+        wp_reset_postdata();
     
     echo "</style>" . "\n";
 }
-
+add_action( 'wp_head', 'dh_ptp_easy_pricing_table_dynamic_css');
 
 /**
  * This function decides which table style we should create. It enqueue the appropriate CSS file and calls the appropriate function.
@@ -51,7 +36,7 @@ function dh_ptp_generate_pricing_table($id)
     $meta = get_post_meta($id, $features_metabox->get_the_id(), TRUE);
 
     //include css
-    wp_enqueue_style( 'simple-flat-table-style', PTP_PLUGIN_PATH_FOR_SUBDIRS . '/assets/pricing-tables/simple-flat/pricingtable.css'  );
+    wp_enqueue_style( 'dh-ptp-design1', PTP_PLUGIN_PATH_FOR_SUBDIRS . '/assets/pricing-tables/design1/pricingtable.css' );
 
     //call appropriate function
     return dh_ptp_generate_simple_flat_pricing_table_html($id);
