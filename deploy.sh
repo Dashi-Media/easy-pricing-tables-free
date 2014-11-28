657b717205cd7559afba9c0c8b3582e91cef60ef
davidhme <david.hehenberger@gmail.com>
2014-11-28 13:30:16 +0700

    latest

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ MAINFILE="pricing-table-plugin.php" # this should be the name of your main php f
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
  
 # svn config
-SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
+SVNPATH="/tmp/deploy/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="davidhme" # your svn username
  
