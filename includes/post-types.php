908d716a8b70b74de0b3f10486ee8056c138f9f1
Ryan Novotny <ryan.novotny@gmail.com>
2016-03-30 16:52:32 -0700

    add missing semicolon

--- includes/post-types.php
+++ includes/post-types.php
@@ -83,7 +83,7 @@ function dh_ptp_custom_rewrites($translation, $text, $domain)
 	global $post;
 	
 	if ( ! isset( $post->post_type ) || empty ( $text ) ) {
-    	return $translation
+    	return $translation;
     }
 	
 	$translations = get_translations_for_domain($domain);
