e3d79c45f5555872192ed8e4bb1a66560cf70574
davidhme <david.hehenberger@gmail.com>
2016-06-13 13:22:05 -0700

    updated sidebar ad + plugin name

--- pricing-table-plugin.php
+++ pricing-table-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Plugin Name: Easy Pricing Tables Lite by Fatcat Apps
+	Plugin Name: Easy Pricing Tables by Fatcat Apps
 	Plugin URI: https://fatcatapps.com/easypricingtables
 	Description: Create a Beautiful, Responsive and Highly Converting Pricing or Comparison Table in Less Than 5 Minutes with Easy Pricing Tables for WordPress. No Coding Required.
 	Text Domain: easy-pricing-tables
