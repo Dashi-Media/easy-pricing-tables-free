35399b451bb19c8aabc245a6201bb612868a6da6
Ryan Novotny <ryan.novotny@gmail.com>
2018-11-30 14:28:04 +0700

    fix styles

--- includes/block.php
+++ includes/block.php
@@ -12,17 +12,18 @@ function dh_ptp_gutenblock() {
 		array( 'wp-blocks', 'wp-element', 'wp-editor' )
 	);
 	wp_register_style( 'dh-ptp-block-css', PTP_PLUGIN_URL . '/assets/ui/block.css' );
-	
+	wp_register_style( 'dh-ptp-design1', PTP_PLUGIN_URL . '/assets/pricing-tables/design1/pricingtable.min.css' );
+		
 	if ( function_exists( 'register_block_type' ) ) {
 		register_block_type( 'easy-pricing-tables/gutenblock',
 			array(
 				'editor_script' => 'dh_ptp_gutenblock_script',
-				'editor_style' => 'dh-ptp-block-css',
+				'editor_style' => array( 'dh-ptp-block-css', 'dh-ptp-design1' ),
 				'render_callback' => 'dh_ptp_gutenblock_render',
 				'attributes' => array( 
 					'post_id' => array( 
-						'type' => 'number',
-						'default' => 0				
+						'type' => 'string',
+						'default' => '0'				
 					)
 				)
 			)
