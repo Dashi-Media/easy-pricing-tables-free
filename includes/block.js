eb77f793558e375884eac384dcf6fa78d8ef52b7
Ryan Novotny <ryan.novotny@gmail.com>
2019-01-04 10:26:35 +0700

    wrap block in fragment

--- includes/block.js
+++ includes/block.js
@@ -5,6 +5,7 @@
 	var BlockControls = editor.BlockControls
 	var SelectControl = wp.components.SelectControl
 	var Toolbar = wp.components.Toolbar
+	var Fragment = wp.element.Fragment
 	var tables = dh_ptp_gutenblock_script_data.tables
 
 	blocks.registerBlockType( 'easy-pricing-tables/gutenblock', {
@@ -14,44 +15,38 @@
 		keywords: ['pricing', 'table', 'tables' ],
 		edit: function( props ) {
 			return [
-				createElement(
-					BlockControls,
-					{ 
-						key: 'controls'
-					},		
+				createElement( Fragment, {},
 					createElement(
-						SelectControl,
-						{	
-							className: 'dh-ptp-gutenblock-select',
-							value: props.attributes.post_id,
-							options: tables,
-							onChange: function( newValue ){ props.setAttributes({ post_id: newValue }) }
-						}
+						BlockControls, {},		
+						createElement(
+							SelectControl, {	
+								className: 'dh-ptp-gutenblock-select',
+								value: props.attributes.post_id,
+								options: tables,
+								onChange: function( newValue ){ props.setAttributes({ post_id: newValue }) }
+							}
+						),
+						props.attributes.post_id == 0 ? '' : 
+						createElement( 'a', {	
+								href: dh_ptp_gutenblock_script_data.editurl + '?post=' + props.attributes.post_id + '&action=edit',
+								target: '_blank',
+								className: 'dh-ptp-gutenblock-link'
+							},
+							'Edit'
+						),
+						createElement( 'a', {	
+								href: dh_ptp_gutenblock_script_data.newurl + '?post_type=easy-pricing-table',
+								target: '_blank',
+								className: 'dh-ptp-gutenblock-link'
+							},
+							'New'
+						)
 					),
-					props.attributes.post_id == 0 ? '' : 
-					createElement(
-						'a',
-						{	
-							href: dh_ptp_gutenblock_script_data.editurl + '?post=' + props.attributes.post_id + '&action=edit',
-							target: '_blank',
-							className: 'dh-ptp-gutenblock-link'
-						},
-						'Edit'
-					),
-					createElement(
-						'a',
-						{	
-							href: dh_ptp_gutenblock_script_data.newurl + '?post_type=easy-pricing-table',
-							target: '_blank',
-							className: 'dh-ptp-gutenblock-link'
-						},
-						'New'
-					)
-				),
-				createElement( wp.components.ServerSideRender, {
-					block: 'easy-pricing-tables/gutenblock',
-					attributes:  props.attributes,
-				})
+					createElement( wp.components.ServerSideRender, {
+						block: 'easy-pricing-tables/gutenblock',
+						attributes:  props.attributes,
+					})
+				)
 			]
 		},
 
